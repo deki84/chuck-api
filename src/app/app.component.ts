5b1a2d43144e9ba96441c357f281204d3a153fa3
Dejan Jankovic <deki1984@hotmail.de>
2023-02-27 18:38:18 +0100

    create a module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,31 @@
 import { JokesService } from './jokes.service';
 import { Component, OnInit } from '@angular/core';
 
+// Models
+import { Joke } from './joke.module';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  jokes: any[] = [];
-  categories: any[] = [];
+  jokes: Joke[] = [];
+  categories: string[] = [];
 
   constructor(private jokesService: JokesService) {}
 
   ngOnInit() {
-    this.jokesService.getCategories().subscribe((categories: any) => {
+    this.jokesService.getCategories().subscribe((categories: string[]) => {
       this.categories = categories;
     });
     this.jokesService
       .getRandomJoke()
-      .subscribe((joke: any) => this.jokes.push(joke));
+      .subscribe((joke: Joke) => this.jokes.push(joke));
   }
   // Search by category
   searchByCategory(category: string) {
-    this.jokesService.getCategoryJoke(category).subscribe((joke) => {
+    this.jokesService.getCategoryJoke(category).subscribe((joke: Joke) => {
       this.jokes = [];
       this.jokes.push(joke);
     });
@@ -30,7 +33,7 @@ export class AppComponent implements OnInit {
   // Search by Serach Term
   searchBySearchTerm(searchTerm: string) {
     if (searchTerm !== '') {
-      this.jokesService.getSearchJokes(searchTerm).subscribe((jokes: any) => {
+      this.jokesService.getSearchJokes(searchTerm).subscribe((jokes) => {
         this.jokes = jokes.result;
       });
     }
--- src/app/jokes.service.ts
+++ src/app/jokes.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// Models
+import { Joke } from './joke.module';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,19 +14,21 @@ export class JokesService {
 
   // Get Random Joke
   getRandomJoke() {
-    return this.http.get(this.apiUrl + 'random');
+    return this.http.get<Joke>(this.apiUrl + 'random');
   }
 
   // Get Categories
   getCategories() {
-    return this.http.get(this.apiUrl + 'categories');
+    return this.http.get<string[]>(this.apiUrl + 'categories');
   }
   // Get Joke By Category
   getCategoryJoke(category: string) {
-    return this.http.get(this.apiUrl + `random?category=${category}`);
+    return this.http.get<Joke>(this.apiUrl + `random?category=${category}`);
   }
   // Get Search Jokes
   getSearchJokes(searchTerm: string) {
-    return this.http.get(this.apiUrl + `search?query=${searchTerm}`);
+    return this.http.get<{ result: Joke[]; amount: number }>(
+      this.apiUrl + `search?query=${searchTerm}`
+    );
   }
 }
